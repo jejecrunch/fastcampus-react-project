bd3112352e7065acb213151edf8dea17ab93adce
제제jeje <gkswlgp456@gmail.com>
2022-10-27 23:56:55 +0900

    refactor(chap1): remove refeat code

--- virtual-keyboard/src/js/keyboard.js
+++ virtual-keyboard/src/js/keyboard.js
@@ -2,12 +2,14 @@ export class Keyboard {
   #switchEl;
   #fontSelectEl;
   #containerEl;
-  #keyboardEl;
   #inputGroupEl;
   #inputEl;
+  #keyboardEl;
+
   #keyPress = false;
   #mouseDown = false;
   #shift = false;
+
   constructor() {
     this.#assignElement();
     this.#addEvent();
@@ -17,9 +19,9 @@ export class Keyboard {
     this.#containerEl = document.getElementById("container");
     this.#switchEl = this.#containerEl.querySelector("#switch");
     this.#fontSelectEl = this.#containerEl.querySelector("#font");
-    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
     this.#inputGroupEl = this.#containerEl.querySelector("#input-group");
     this.#inputEl = this.#inputGroupEl.querySelector("#input");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
   }
 
   #addEvent() {
@@ -28,9 +30,9 @@ export class Keyboard {
       "change",
       this.#onChangeFont.bind(this)
     );
+    this.#inputEl.addEventListener("input", this.#onInput);
     document.addEventListener("keydown", this.#onKeyDown.bind(this));
     document.addEventListener("keyup", this.#onKeyUp.bind(this));
-    this.#inputEl.addEventListener("input", this.#onInput);
     this.#keyboardEl.addEventListener(
       "mousedown",
       this.#onMouseDown.bind(this)
@@ -38,38 +40,16 @@ export class Keyboard {
     document.addEventListener("mouseup", this.#onMouseUp.bind(this));
   }
 
-  #onMouseUp(event) {
-    if (this.#keyPress) return;
-    this.#mouseDown = false;
-    const keyEl = event.target.closest("div.key");
-    const isActive = !!keyEl?.classList.contains("active");
-    const val = keyEl?.dataset.val;
-
-    if (event.code === "ShiftLeft") {
-      this.#shift = false;
-    }
-
-    if (isActive && !!val && val !== "Space" && val !== "Backspace") {
-      if (this.#shift) this.#inputEl.value += val.toUpperCase();
-      else this.#inputEl.value += val;
-    }
-    if (isActive && val === "Space") {
-      this.#inputEl.value += " ";
-    }
-    if (isActive && val === "Backspace") {
-      this.#inputEl.value = this.#inputEl.value.slice(0, -1);
-    }
-    this.#keyboardEl.querySelector(".active")?.classList.remove("active");
+  #onChangeTheme(event) {
+    document.documentElement.setAttribute(
+      "theme",
+      event.target.checked ? "dark-mode" : ""
+    );
   }
 
-  #onMouseDown(event) {
-    if (this.#keyPress) return;
-    this.#mouseDown = true;
-
-    if (event.code === "ShiftLeft") {
-      this.#shift = true;
-    }
-    event.target.closest("div.key")?.classList.add("active");
+  #onChangeFont(event) {
+    document.body.style.fontFamily = event.target.value;
+    this.#inputEl.style.fontFamily = event.target.value;
   }
 
   #onInput(event) {
@@ -80,7 +60,7 @@ export class Keyboard {
     if (this.#mouseDown) return;
     this.#keyPress = true;
 
-    if (event.code === "ShiftLeft") {
+    if (event.code === "ShiftLeft" || event.code === "ShiftRight") {
       this.#shift = true;
     }
 
@@ -98,7 +78,7 @@ export class Keyboard {
     if (this.#mouseDown) return;
     this.#keyPress = false;
 
-    if (event.code === "ShiftLeft") {
+    if (event.code === "ShiftLeft" || event.code === "ShiftRight") {
       this.#shift = false;
     }
 
@@ -106,6 +86,34 @@ export class Keyboard {
     const isActive = !!keyEl?.classList.contains("active");
     const val = keyEl?.dataset.val;
 
+    this.#onTyping(isActive, val);
+  }
+
+  #onMouseDown(event) {
+    if (this.#keyPress) return;
+    this.#mouseDown = true;
+
+    if (event.code === "ShiftLeft" || event.code === "ShiftRight") {
+      this.#shift = true;
+    }
+    event.target.closest("div.key")?.classList.add("active");
+  }
+
+  #onMouseUp(event) {
+    if (this.#keyPress) return;
+    this.#mouseDown = false;
+    const keyEl = event.target.closest("div.key");
+    const isActive = !!keyEl?.classList.contains("active");
+    const val = keyEl?.dataset.val;
+
+    if (event.code === "ShiftLeft" || event.code === "ShiftRight") {
+      this.#shift = false;
+    }
+
+    this.#onTyping(isActive, val);
+  }
+
+  #onTyping(isActive, val) {
     if (isActive && !!val && val !== "Space" && val !== "Backspace") {
       if (this.#shift) this.#inputEl.value += val.toUpperCase();
       else this.#inputEl.value += val;
@@ -117,17 +125,5 @@ export class Keyboard {
       this.#inputEl.value = this.#inputEl.value.slice(0, -1);
     }
     this.#keyboardEl.querySelector(".active")?.classList.remove("active");
-  }
-
-  #onChangeTheme(event) {
-    document.documentElement.setAttribute(
-      "theme",
-      event.target.checked ? "dark-mode" : ""
-    );
-  }
-
-  #onChangeFont(event) {
-    document.body.style.fontFamily = event.target.value;
-    this.#inputEl.style.fontFamily = event.target.value;
   }
 }
