b7c914373cc37e58a911d8bf0fe3d62de3a2b133
jejecrunch <gkswlgp456@gmail.com>
2023-03-16 16:23:45 +0900

    refactor(chap3): add comment and remove variables

--- datepicker/src/js/main.js
+++ datepicker/src/js/main.js
@@ -14,55 +14,66 @@ class DatePicker {
     "December",
   ];
 
+  // 오늘
+  #today = new Date();
+
+  // 달력 날짜
   #calendarDate = {
-    data: "",
-    date: 0,
-    month: 0,
-    year: 0,
+    data: this.#today,
+    year: this.#today.getFullYear(),
+    month: this.#today.getMonth(),
+    date: this.#today.getDate(),
   };
 
-  today = new Date();
-
-  selectedDate = {
-    data: this.today,
-    year: this.today.getFullYear(),
-    month: this.today.getMonth(),
-    date: this.today.getDate(),
+  // 선택한 날짜
+  #selectedDate = {
+    ...this.#calendarDate,
   };
 
   constructor() {
+    // 캘린더 날짜 초기화
     this.initCalendarDate();
-    this.initSelectedDate();
-
+    // 요소 가져오고 이벤트 등록
     this.assignElement();
     this.addEvent();
 
+    // 초기화한 날짜로 세팅
     this.setDateInput();
   }
 
-  initSelectedDate() {
-    this.selectedDate = { ...this.#calendarDate };
-  }
-
-  setDateInput() {
-    this.dateInputEl.textContent = this.formatDate(this.selectedDate.data);
-    this.dateInputEl.dataset.value - this.selectedDate.data;
-  }
-
+  // 캘린더 날짜 초기화
   initCalendarDate() {
-    const data = new Date();
-    const date = data.getDate();
-    const month = data.getMonth();
-    const year = data.getFullYear();
+    const date = this.#today.getDate();
+    const month = this.#today.getMonth();
+    const year = this.#today.getFullYear();
 
     this.#calendarDate = {
-      data,
+      data: this.#today,
       date,
       month,
       year,
     };
   }
 
+  // date input 내용 변경
+  setDateInput() {
+    this.dateInputEl.textContent = this.formatDate(this.#selectedDate.data);
+    this.dateInputEl.dataset.value - this.#selectedDate.data;
+  }
+  // 날짜 데이터 포매팅
+  formatDate(dateData) {
+    let date = dateData.getDate();
+    if (date < 10) date = `0${date}`;
+
+    let month = dateData.getMonth() + 1;
+    if (month < 10) month = `0${month}`;
+
+    let year = dateData.getFullYear();
+
+    return `${year}/${month}/${date}`;
+  }
+
+  // 요소 연결
   assignElement() {
     this.datePickerEl = document.getElementById("date-picker");
     this.dateInputEl = this.datePickerEl.querySelector("#date-input");
@@ -74,6 +85,7 @@ class DatePicker {
     this.calendarDatesEl = this.calendarEl.querySelector("#dates");
   }
 
+  // 이벤트 등록
   addEvent() {
     this.dateInputEl.addEventListener("click", this.toggleCalendar.bind(this));
     this.nextBtnEl.addEventListener("click", this.moveToNextMonth.bind(this));
@@ -84,10 +96,11 @@ class DatePicker {
     );
   }
 
+  // 날짜 선택할 때의 이벤트 함수
   onClickSelectDate(event) {
     const eventTarget = event.target;
     if (eventTarget.dataset.date) {
-      this.selectedDate = {
+      this.#selectedDate = {
         data: new Date(
           this.#calendarDate.year,
           this.#calendarDate.month,
@@ -109,18 +122,7 @@ class DatePicker {
     }
   }
 
-  formatDate(dateData) {
-    let date = dateData.getDate();
-    if (date < 10) date = `0${date}`;
-
-    let month = dateData.getMonth() + 1;
-    if (month < 10) month = `0${month}`;
-
-    let year = dateData.getFullYear();
-
-    return `${year}/${month}/${date}`;
-  }
-
+  // 이전달로 넘어가는 함수
   moveToPrevMonth() {
     this.#calendarDate.month--;
     if (this.#calendarDate.month < 0) {
@@ -131,6 +133,7 @@ class DatePicker {
     this.updateDates();
   }
 
+  // 다음달로 넘어가는 함수
   moveToNextMonth() {
     this.#calendarDate.month++;
     if (this.#calendarDate.month > 11) {
@@ -141,21 +144,24 @@ class DatePicker {
     this.updateDates();
   }
 
+  // 달력 열기 함수
   toggleCalendar() {
     if (this.calendarEl.classList.contains("active")) {
-      this.#calendarDate = { ...this.selectedDate };
+      this.#calendarDate = { ...this.#selectedDate };
     }
     this.calendarEl.classList.toggle("active");
     this.updateMonth();
     this.updateDates();
   }
 
+  // 달력 조작 시 월이 바뀌는 함수
   updateMonth() {
     this.monthContentEl.textContent = `${this.#calendarDate.year} ${
       this.monthData[this.#calendarDate.month]
     }`;
   }
 
+  // 달력 조작 시 날짜 바뀌는 함수
   updateDates() {
     this.calendarDatesEl.innerHTML = "";
     const numberOfDates = new Date(
@@ -181,33 +187,31 @@ class DatePicker {
     this.markSelectedDate();
   }
 
+  // 선택한 날짜 칠하기
   markSelectedDate() {
     if (
-      this.selectedDate.year === this.#calendarDate.year &&
-      this.selectedDate.month === this.#calendarDate.month
+      this.#selectedDate.year === this.#calendarDate.year &&
+      this.#selectedDate.month === this.#calendarDate.month
     ) {
       this.calendarDatesEl
-        .querySelector(`[data-date='${this.selectedDate.date}']`)
+        .querySelector(`[data-date='${this.#selectedDate.date}']`)
         .classList.add("selected");
     }
   }
 
+  // 오늘 칠하기
   markToday() {
-    const currentDate = new Date();
-    const currentMonth = currentDate.getMonth();
-    const currrentYear = currentDate.getFullYear();
-    const today = currentDate.getDate();
-
     if (
-      currrentYear === this.#calendarDate.year &&
-      currentMonth === this.#calendarDate.month
+      this.#today.getFullYear() === this.#calendarDate.year &&
+      this.#today.getMonth() === this.#calendarDate.month
     ) {
       this.calendarDatesEl
-        .querySelector(`[data-date='${today}']`)
+        .querySelector(`[data-date='${this.#today.getDate()}']`)
         .classList.add("today");
     }
   }
 
+  // 토요일 글자색 변경
   colorSaturday() {
     const saturdayEls = this.calendarDatesEl.querySelectorAll(
       `.date:nth-child(7n+${
@@ -220,6 +224,7 @@ class DatePicker {
     }
   }
 
+  // 일요일 글자색 변경
   colorSunday() {
     const sundayEls = this.calendarDatesEl.querySelectorAll(
       `.date:nth-child(7n+${
